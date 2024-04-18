63c542568d6f2dff00faf3d0feb56cfbd1f32341
Robby Doyle <rmdoyledesign@gmail.com>
2024-04-18 14:19:08 -0700

    clean up globes / add footer / add nav

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-slate-100 text-center p-4">
+      <p>Socialite | 2024®</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+const Nav = () => {
+  const links = [
+    {
+      title: "posts",
+      href: "/posts",
+    },
+    {
+      title: "friends",
+      href: "/friends",
+    },
+    {
+      title: "profile",
+      href: "/profile",
+    },
+  ];
+  return (
+    <header className=" bg-slate-100 p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link
+          aria-label="Go To Home"
+          href="/"
+          className="bg-indigo-500 p-2 rounded-lg text-white font-bold"
+        >
+          Socialite
+        </Link>
+        <nav className="flex items-center gap-4">
+          {links.map((link) => (
+            <Link
+              key={link.title}
+              className="font-bold hover:text-indigo-600 hover:underline capitalize"
+              href={link.href}
+            >
+              {link.title}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Nav;
