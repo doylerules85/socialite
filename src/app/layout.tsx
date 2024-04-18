d6af916197db93ef746a064ca1a05004469d6384
Robby Doyle <rmdoyledesign@gmail.com>
2024-04-18 14:19:15 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Nav />
-        {children}
+        <main className="min-h-full">{children}</main>
+        <Footer />
       </body>
     </html>
   );
